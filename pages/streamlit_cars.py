f194dd20da5e6814c2fc70788d3bf6d70de09cd4
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-21 16:47:04 +0100

    Add files via upload

---
+++ pages/streamlit_cars.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout="wide")
+
+title1, title2 = st.columns(2)
+
+with title1 :
+  st.title('Julie D - Streamlit : build and share data apps')
+with title2 :
+  st.image('logo_WCS.png')
+
+st.title("Analyse de l'ensemble des données :")
+
+colA, colB = st.columns(2)
+
+with colA :
+  df = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv')
+  df
+
+with colB :
+  sns.heatmap(df.corr(), cmap = 'Spectral_r', center = 0, annot = True).figure
+  plt.close()
+
+st.title('Quelques statistiques globales de nos données :')
+
+f"Nombre d'entrées : {len(df)}."
+
+stats = pd.DataFrame(df.describe())
+stats = stats.drop(['count'], axis = 0).round().astype('int')
+stats
+
+
+st.title('Visualisation :')
+
+col1, col2, col3 = st.columns(3)
+
+with col1 :
+  sns.boxplot(x='continent', y = 'mpg', data = df).figure
+  plt.close()
+
+  sns.boxplot(x='continent', y = 'cylinders', data = df).figure
+  plt.close()
+
+with col2 :
+  sns.boxplot(x='continent', y = 'cubicinches', data = df).figure
+  plt.close()
+
+  sns.boxplot(x='continent', y = 'hp', data = df).figure
+  plt.close()
+
+with col3 :
+  sns.boxplot(x='continent', y = 'time-to-60', data = df).figure
+  plt.close()
+
+  sns.boxplot(x='continent', y = 'year', data = df).figure
+  plt.close()
---
+++ pages/streamlit_cars_region.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout="wide")
+
+title1, title2 = st.columns(2)
+
+with title1 :
+  st.title('Julie D - Streamlit : build and share data apps')
+with title2 :
+  st.image('logo_WCS.png')
+
+st.title('Analyse en fonction de la région :')
+
+df = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv')
+
+options = list(df['continent'].unique())
+choix_region = st.selectbox('Choisissez la région :', options)
+df_region = df[(df['continent'] == choix_region)]
+df_region
+
+#var = ['mpg', 'cylinders', 'cubicinches', 'hp', 'weightlbs', 'time-to-60', 'year']
+
+#for i in var :  
+#  sns.boxplot(x='continent', y = i, data = df_region).figure
+#  plt.close()
+
+st.title('Quelques statistiques globales de nos données :')
+
+nb = len(df_region)
+f"La région {choix_region} comprend {nb} entrées, ce qui représente environ {round(nb / len(df) *100)} % de l'ensemble de nos données"
+
+stats = pd.DataFrame(df_region.describe())
+stats = stats.drop(['count'], axis = 0).round().astype('int')
+stats
+
+st.title('Visualisation :')
+
+col1, col2, col3 = st.columns(3)
+
+with col1 :
+  sns.boxplot(x='continent', y = 'mpg', data = df_region).figure
+  plt.close()
+
+  sns.boxplot(x='continent', y = 'cylinders', data = df_region).figure
+  plt.close()
+
+with col2 :
+  sns.boxplot(x='continent', y = 'cubicinches', data = df_region).figure
+  plt.close()
+
+  sns.boxplot(x='continent', y = 'hp', data = df_region).figure
+  plt.close()
+
+with col3 :
+  sns.boxplot(x='continent', y = 'time-to-60', data = df_region).figure
+  plt.close()
+
+  sns.boxplot(x='continent', y = 'year', data = df_region).figure
+  plt.close()
